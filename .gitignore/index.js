adae46bae1f27524227b0c1ec7c7c1f36eecc5bf
kyzen <46655542+kyzen@users.noreply.github.com>
2019-01-13 20:50:36 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -58,3 +58,41 @@ client.on('message',message =>{
        message.channel.send(":white_check_mark: **"+member.user.username + '** a été banni par **' + message.author.username + '**')
     }
 });
+
+client.on("message", message => {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLowerCase() === prefix + "clear") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(":x: **Erreur** ! Raison : Permissions manquantes")
+        let count = args[1]
+        if (!count) return message.channel.send(":x: **Erreur** ! Raison : veuillez indiquer un nombre de messages à supprimer")
+        if (isNaN(count)) return message.channel.send(":x: **Erreur** ! Raison : nombre invalide")
+        if (count < 1 || count > 100) return message.channel.send(":x: **Erreur** ! Raison : le nombre doit être compris entre 1 et 100")
+        message.channel.bulkDelete(parseInt(count) + 1)
+    }
+ 
+    if (args[0].toLowerCase() === prefix + "mute") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(":x: **Erreur** ! Raison : Permissions manquantes")
+        let member = message.mentions.members.first()
+        if (!member) return message.channel.send(":x: **Erreur** ! Raison : Membre introuvable")
+        if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("Vous ne pouvez pas mute ce membre")
+        if (member.highestRole.calculatedPosition >= message.guild.me.highestRole.calculatedPosition || member.id === message.guild.ownerID) return message.channel.send("Je ne peux pas mute ce membre")
+        let muterole = message.guild.roles.find(role => role.name === 'Muted')
+        if (muterole) {
+            member.addRole(muterole)
+            message.channel.send(':white_check_mark: **' + member + '** a été mute')
+        }
+        else {
+            message.guild.createRole({name: 'Muted', permissions: 0}).then((role) => {
+                message.guild.channels.filter(channel => channel.type === 'text').forEach(channel => {
+                    channel.overwritePermissions(role, {
+                        SEND_MESSAGES: false
+                    })
+                })
+                member.addRole(role)
+                message.channel.send(':white_check_mark: **' + member + '**  a été mute')
+            })
+        }
+    }
+})
